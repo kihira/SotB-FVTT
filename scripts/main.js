7860cd685d242e2ff84d256097dcc191572d9621
kihira <zoe@kihira.uk>
2021-01-31 11:58:41 +0000

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+import { ActorSheetFFG } from "../../../systems/starwarsffg/modules/actors/actor-sheet-ffg.js";
+
+class CyberpunkActorSheet extends ActorSheetFFG
+{
+    /**
+     * @override
+     */
+    static get defaultOptions()
+    {
+        return mergeObject(super.defaultOptions, {
+            classes: ["starwarsffg", "sheet", "actor", "cyberpunk"],
+            template: "modules/shadowofthebeanstalk/templates/actor.html",
+            width: 710,
+            height: 650,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "characteristics" }],
+            scrollY: [".tableWithHeader", ".tab", ".skillsGrid"],
+        });
+    }
+
+    /**
+     * @override
+     */
+    get template()
+    {
+        return `modules/shadowofthebeanstalk/templates/actor.html`;
+    }
+}
+
+Actors.registerSheet("starwarsffg", CyberpunkActorSheet,
+{
+    types: ["character"],
+    label: "Shadow of the Beanstalk",
+});
